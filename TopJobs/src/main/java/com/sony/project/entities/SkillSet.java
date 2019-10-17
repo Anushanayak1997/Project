9ce5e402431011d723e4296d25e4218ad3a18dc2
Monisha <monishakrishnan0904@gmail.com>
2019-10-17 14:00:24 +0530

    companydetails.java skillset.java added

--- TopJobs/src/main/java/com/sony/project/entities/SkillSet.java
+++ TopJobs/src/main/java/com/sony/project/entities/SkillSet.java
@@ -3,7 +3,7 @@ package com.sony.project.entities;
 public class SkillSet {
 	private int skillId;
 	private String skillName;
-	public int getSkillId() {
+	public int getSkillId() { 
 		return skillId;
 	}
 	public void setSkillId(int skillId) {
