d1411772ebb236962edf49453497a6e5ad7a19e0
Monisha <monishakrishnan0904@gmail.com>
2019-10-17 12:27:55 +0530

    RestConfig pull

--- TopJobs/src/main/java/com/sony/project/main/MainApp.java
+++ TopJobs/src/main/java/com/sony/project/main/MainApp.java
@@ -7,7 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author 7000020364
+ * @author Monisha
  *
  */
 @SpringBootApplication
