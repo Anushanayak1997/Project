9187863d08fa2f013cac8f8c3ca9e07c2df15340
Anusha <anushanayak710@gmail.com>
2019-10-17 11:36:24 +0530

    Initial Commit 1) SpringBootApplication created 2) resources configured in Application.properties 3) Created test class, but not implemented.

---
+++ TopJobs/src/main/java/com/sony/project/main/MainApp.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.sony.project.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 7000020364
+ *
+ */
+@SpringBootApplication
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+	}
+
+}
---
+++ TopJobs/src/test/java/com/sony/project/test/MainTest.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.sony.project.test;
+
+/**
+ * @author 7000020364
+ *
+ */
+public class MainTest {
+
+}
