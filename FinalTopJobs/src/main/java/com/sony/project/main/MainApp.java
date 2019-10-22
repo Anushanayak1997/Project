cb7cd5429f9f1d714027c26c8acc5d2d70153d49
Anusha <anushanayak710@gmail.com>
2019-10-22 16:35:02 +0530

    Trying to fix error

--- FinalTopJobs/src/main/java/com/sony/project/main/MainApp.java
+++ FinalTopJobs/src/main/java/com/sony/project/main/MainApp.java
@@ -3,9 +3,14 @@ package com.sony.project.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.sony.project.dao")
 @ComponentScan(basePackages={"com.sony.project.controllers","com.sony.project.services"})
+
+
 public class MainApp {
 
 	public static void main(String[] args) {
