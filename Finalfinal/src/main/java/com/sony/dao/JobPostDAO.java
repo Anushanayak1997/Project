6d65698da70493dad1b2fc5c05100fff381751f5
Monisha <monishakrishnan0904@gmail.com>
2019-10-23 14:38:10 +0530

    dummy

---
+++ Finalfinal/src/main/java/com/sony/dao/JobPostDAO.java
@@ -0,0 +1,12 @@
+package com.sony.dao;
+
+import java.util.List;
+
+import com.sony.model.entity.JobPostEntity;
+
+public interface JobPostDAO {
+
+	List<JobPostEntity> getAllJobs();
+
+	boolean addJobPost(JobPostEntity jobpostentity);
+}
