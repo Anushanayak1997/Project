438d4e68c97af063ef51a0d0c338c8419316702e
Namrata <nammu@gmail.com>
2019-10-25 10:10:26 +0530

    Jobseeker added

---
+++ Finalfinal/src/main/java/com/sony/model/entity/JobSeekerProject.java
@@ -0,0 +1,33 @@
+package com.sony.model.entity;
+
+public class JobSeekerProject {
+
+	private String title;
+	private String description;
+	private String role;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
