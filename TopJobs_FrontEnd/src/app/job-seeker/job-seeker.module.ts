eddc74e4c6db080d149b4c99426fb869da525ab6
Sajjan <Akheel.Sajjan@sony.com>
2019-10-25 15:10:21 +0530

    seer Profile

--- TopJobs_FrontEnd/src/app/job-seeker/job-seeker.module.ts
+++ TopJobs_FrontEnd/src/app/job-seeker/job-seeker.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {FormsModule } from '@angular/forms'
 
 import { ApplicationStatusComponent } from './application-status/application-status.component';
 import { ManageAccountComponent } from './manage-account/manage-account.component';
@@ -14,7 +16,9 @@ import { SeekerProfileComponent } from './seeker-profile/seeker-profile.componen
     SeekerPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule,
+    FormsModule,
   ]
 })
 export class JobSeekerModule { }
