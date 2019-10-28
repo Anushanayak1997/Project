52941ead0bc882c3937504fc50b22b00504a387f
Monisha <monishakrishnan0904@gmail.com>
2019-10-28 08:21:54 +0530

    dummy

--- Finalfinal/src/main/java/com/sony/controller/JobSeekerSkillController.java
+++ Finalfinal/src/main/java/com/sony/controller/JobSeekerSkillController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.sony.model.entity.JobSeekerSkills;
 import com.sony.model.service.JobSeekerSkillService;
 
-@RestController
+@RestController 
 public class JobSeekerSkillController {
 	@Autowired
 	public JobSeekerSkillService jobseekerskillservice;
