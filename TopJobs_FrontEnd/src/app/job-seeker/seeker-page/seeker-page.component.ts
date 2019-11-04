c8198e1dfde9b9d48105b9e7a823d37fe6a8e449
Nayak <Anusha.Nayak@sony.com>
2019-11-04 20:05:41 +0530

    added disabling job application

--- TopJobs_FrontEnd/src/app/job-seeker/seeker-page/seeker-page.component.ts
+++ TopJobs_FrontEnd/src/app/job-seeker/seeker-page/seeker-page.component.ts
@@ -11,21 +11,23 @@ import { RouteConfigLoadEnd, Router } from '@angular/router';
 export class SeekerPageComponent implements OnInit {
 
   userId = sessionStorage.getItem('user_id');
-  url1 = environment.apiBaseUrl + "getuserbyid/1";
+  url1 = environment.apiBaseUrl + "getuserbyid/" + this.userId;
   url2 = environment.apiBaseUrl + "getalljobs";
   url3 = environment.apiBaseUrl + "applyjobpost";
+  url4 = environment.apiBaseUrl + "getapplicantsbyuserid/" + this.userId;
   User:any;
   seekerjobpost: any;
   jobDetails: any;
   userType:any;
+  Status: any = [];
 
   constructor(private http:HttpClient,private router:Router) { }
 
   ngOnInit() {
-
    this.userType=sessionStorage.getItem('user_type');
     if(this.userType == 'JobSeeker'){
-      console.log("correct user");
+      console.log("correct user", this.userId);
+      this.getSeekerStatus();
       this.getJobSeeker();
       this.getJobs();
     }else{
@@ -45,7 +47,22 @@ export class SeekerPageComponent implements OnInit {
     this.http.get(this.url2).subscribe(
       (Response)=>{
         this.jobDetails = Response;
-        console.log("Job POsts", this.jobDetails)
+        for(let i=0;i<this.jobDetails.length;i++) {
+          for(let j=0;j<this.Status.length;j++) {
+            if(this.jobDetails[i].jobPostId == this.Status[j].jobpost.jobPostId)
+              this.jobDetails[i].status = 1;
+          }
+        }
+        console.log("Job Posts", this.jobDetails)
+      }
+    )
+  }
+
+  getSeekerStatus() {
+    this.http.get(this.url4).subscribe(
+      (Response)=>{
+        this.Status = Response;
+        console.log("Status", this.Status);
       }
     )
   }
@@ -55,7 +72,7 @@ export class SeekerPageComponent implements OnInit {
     this.seekerjobpost = {
       'userId':sessionStorage.getItem('user_id'),
       'jobpostId': jobpostid,
-      'status': 'Apply'
+      'status': 'Applied'
     }
     console.log(this.seekerjobpost);
     this.http.post(this.url3,this.seekerjobpost).subscribe(
